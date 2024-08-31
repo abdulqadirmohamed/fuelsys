f0123cefacc2576666821ae844aa8b1a54a9a5ed
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-08-31 12:24:30 +0300

    add tailwindcss

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,21 +2,21 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { AddFuelComponent } from '../add-fuel/add-fuel.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDialog ],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  constructor(private _dialog:MatDialog){
 
-  }
-  openAddFuel(){
-    this._dialog.open(AddFuelComponent)
+  constructor(private _dialog: MatDialog) {}
+
+  openAddFuel() {
+    this._dialog.open(AddFuelComponent);
   }
 }
