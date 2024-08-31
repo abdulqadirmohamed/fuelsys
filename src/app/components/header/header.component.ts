35f1b3b52853d52975542a5017c76b7bdeaccb91
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-08-31 07:36:48 +0300

    first commit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatDialogModule} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
+import { AddFuelComponent } from '../add-fuel/add-fuel.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDialogModule ],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDialog ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private _dialog:MatDialog){
 
+  }
+  openAddFuel(){
+    this._dialog.open(AddFuelComponent)
+  }
 }
