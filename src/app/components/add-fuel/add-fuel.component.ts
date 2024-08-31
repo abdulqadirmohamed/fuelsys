03b9cf812ba1cdc231a18abb4b919b4b904a3c0f
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-08-31 07:36:03 +0300

    first commit

---
+++ src/app/components/add-fuel/add-fuel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-fuel',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-fuel.component.html',
+  styleUrl: './add-fuel.component.css'
+})
+export class AddFuelComponent {
+
+}
